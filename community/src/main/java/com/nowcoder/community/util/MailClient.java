7fcf7c1cbe961ac75bfc83fb33f717c24c68d591
Chutong Ren <chutong.ren@tum.de>
2025-02-27 20:01:08 +0100

    27.02.2025

---
+++ community/src/main/java/com/nowcoder/community/util/MailClient.java
@@ -0,0 +1,14 @@
+package com.nowcoder.community.util;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class MailClient {
+    private static final Logger logger = LoggerFactory.getLogger(MailClient.class);
+
+    
+}
